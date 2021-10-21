eff7400032d81ed14b63f71c8b28be86ce0656be
Jerem <jersou@gmail.com>
2021-10-21 22:51:59 +0200

    feat: add fromString test

--- shell_stream_test.ts
+++ shell_stream_test.ts
@@ -1,6 +1,12 @@
 import { assertEquals } from "./test_deps.ts";
 import { map } from "./operators/map.ts";
-import { FromArray, FromFile, FromRun, Pipe } from "./shell_stream.ts";
+import {
+  FromArray,
+  FromFile,
+  FromRun,
+  FromString,
+  Pipe,
+} from "./shell_stream.ts";
 import { fromArray } from "./startpoints/from_array.ts";
 import { log } from "./operators/log.ts";
 
@@ -44,6 +50,16 @@ Deno.test("FromRun", async () => {
     'console.log("lineFromRun")',
   ]).toString();
   assertEquals(out, "lineFromRun");
+});
+
+Deno.test("FromArray", async () => {
+  const res = await FromArray(["line1", "line2"]).toArray();
+  assertEquals(res, ["line1", "line2"]);
+});
+
+Deno.test("FromString", async () => {
+  const res = await FromString("line1\nline2").toArray();
+  assertEquals(res, ["line1", "line2"]);
 });
 
 Deno.test("run", async () => {
