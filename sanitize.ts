88e3a7c0d0bc17172b2567b0d13eb854a3ebdf2e
Jerem <jersou@gmail.com>
2021-10-21 22:51:39 +0200

    feat: sanitize return boolean

--- sanitize.ts
+++ sanitize.ts
@@ -3,6 +3,7 @@ import { bgRed } from "./deps.ts";
 const stdRes = ["stdin", "stderr", "stdout"];
 
 export function checkResources() {
+  let noOpenedRessource = true;
   const res = Deno.resources();
   if (
     Object.keys(res).length !== 3 &&
@@ -12,10 +13,13 @@ export function checkResources() {
       bgRed("Some resources are not closed except stdin/stderr/stdout :"),
     );
     console.log(res);
+    noOpenedRessource = false;
   }
+  return noOpenedRessource;
 }
 
 export function checkOps() {
+  let noOpsInProgress = true;
   const metrics = Deno.metrics();
 
   const opsMetricsNameGroups: [keyof Deno.Metrics, keyof Deno.Metrics][] = [
@@ -27,20 +31,23 @@ export function checkOps() {
   ];
 
   opsMetricsNameGroups.forEach((group) => {
-    const dispached = group[0];
+    const dispatched = group[0];
     const completed = group[1];
-    if (metrics[dispached] !== metrics[completed]) {
+    if (metrics[dispatched] !== metrics[completed]) {
       console.log(
         bgRed(
-          `${metrics[dispached]} ${dispached} ` +
+          `${metrics[dispatched]} ${dispatched} ` +
             `!== ${metrics[completed]} ${completed}`,
         ),
       );
+      noOpsInProgress = false;
     }
   });
+  return noOpsInProgress;
 }
 
 export function sanitize() {
-  checkResources();
-  checkOps();
+  const noOpenedRessource = checkResources();
+  const noOpsInProgress = checkOps();
+  return noOpenedRessource && noOpsInProgress;
 }
